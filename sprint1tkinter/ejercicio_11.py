b147780f30ba93f2957d8cfe265cfac20a4b13f6
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2024-10-18 17:16:47 +0200

    Completado ejercicio 11

---
+++ sprint1tkinter/ejercicio_11.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+def update_value(value):
+    label.config(text = "Valor: " + value)
+
+
+
+root = tk.Tk()
+root.title("Ejercicio 11")
+root.geometry("400x200")
+
+
+# Creación scale
+scale = tk.Scale(root, from_ = 0, to = 100, orient = "horizontal", command = update_value)
+scale.pack(pady = 20)
+
+
+# Label para mostrar valor
+label = tk.Label(root, text = "Valor: 0")
+label.pack(pady = 10)
+
+
+root.mainloop()
