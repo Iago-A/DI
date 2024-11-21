502aac1040b62ccc17a59baaa0e711d9254e14a4
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2024-11-21 09:35:50 +0100

    Completado ejercicio 3

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,5 +1,24 @@
 package com.example.mycatalog;
 
-public class DetailActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        ImageView imageView = findViewById(R.id.catImageView);
+        Glide.with(this)
+                .load(R.drawable.black_cat)
+                .apply(RequestOptions.circleCropTransform())
+                .into(imageView);
+    }
 }
