f11e2fef2c9538e2e87e5f78444a60c9c6569402
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2025-01-16 19:20:11 +0100

    LoginActivity & Register Activity added

---
+++ AeroCatalogo/app/src/main/java/com/example/aerocatalogo/LoginActivity.java
@@ -0,0 +1,52 @@
+package com.example.aerocatalogo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.loginButton).setOnClickListener(v -> loginUser());
+        findViewById(R.id.createAccountButton).setOnClickListener(v -> createNewUser());
+    }
+
+    private void loginUser() {
+        String email = ((EditText) findViewById(R.id.emailEditText)).getText().toString();
+        String password = ((EditText) findViewById(R.id.passwordEditText)).getText().toString();
+
+        // Validar campos rellenos
+        if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please, complete all fields.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login successful.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(LoginActivity.this, "Error in the authentication.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void createNewUser() {
+        Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+}
